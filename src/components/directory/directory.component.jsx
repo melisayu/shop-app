430be583f086aaa97b61bfb1a9bea00ba21a0fa6
Melisa <melisayuliakristin@gmail.com>
2019-12-14 16:37:40 +0700

    Add Shop Page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,28 +13,33 @@ class Directory extends React.Component {
                 title: 'hats',
                 imageUrl: 'https://images.pexels.com/photos/1878821/pexels-photo-1878821.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                 id: 1,
+                linkUrl: 'hats',
             },
             {
                 title: 'backpacks',
                 imageUrl: 'https://images.pexels.com/photos/843194/pexels-photo-843194.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                 id: 2,
+                linkUrl: '',
             },
             {
                 title: 'sneakers',
                 imageUrl: 'https://images.pexels.com/photos/1240892/pexels-photo-1240892.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                 id: 3,
+                linkUrl: '',
             },
             {
                 title: 'womens',
-                imageUrl: 'https://images.pexels.com/photos/1852382/pexels-photo-1852382.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                imageUrl: 'https://images.pexels.com/photos/1375849/pexels-photo-1375849.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                 size: 'large',
                 id: 4,
+                linkUrl: '',
             },
             {
                 title: 'mens',
                 imageUrl: 'https://images.pexels.com/photos/1384218/pexels-photo-1384218.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                 size: 'large',
                 id: 5,
+                linkUrl: '',
             }]
         }
     }
@@ -43,8 +48,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({ title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
